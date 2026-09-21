7d616eab1f56f310b5e60b5288fbb4528525bc9e
Callum <callum.o.stevens@gmail.com>
2026-09-21 11:18:17 +0100

    misc: Public beta build changes

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -128,8 +128,7 @@ export default function Header() {
 					disabled={!takeScreenshot}
 					onClick={() => takeScreenshot?.()}
 				/>
-				{/*<BoundaryIndicator boundaryName={boundaryName} />*/}
-				<a
+				{/*<a
 					href="https://github.com/Keepercal/streets-dashboard"
 					target="_blank"
 					rel="noopener noreferrer"
@@ -138,7 +137,7 @@ export default function Header() {
 					title={`Open ${__APP_NAME__} GitHub repository`}
 				>
 					<img src="./github-mark.svg" alt="GitHub" />
-				</a>
+				</a>*/}
 			</div>
 		</div>
 	);
